dbbf13f7028e4fa97d958781adeeff2ff5d8d4a8
Austin <austin.dober@gmail.com>
2019-09-21 11:10:37 -0400

    adding e-mailing capabilities

--- server.js
+++ server.js
@@ -2,11 +2,24 @@
 // =============================================================
 var express = require("express");
 var path = require("path");
-
 // Sets up the Express App
 // =============================================================
 var app = express();
 var PORT = process.env.PORT || 3000;
+
+//emailing when ready
+var mailer = require('node-mailer');
+
+new mailer.Mail({
+	from: 'noreply@domain.com',
+	to: 'rodolphe@domain.com',
+	subject: 'My Subject',
+	body: 'My body',
+	callback: function(err, data){
+		console.log(err);
+		console.log(data);
+	}
+});
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
